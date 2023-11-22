66fdbba9ef5dc0a9d273bc90982ced398bd7e0c0
reth <gabrielwastha@gmail.com>
2023-11-22 01:22:22 -0300

    save

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,13 @@ import * as express from "express"
 
 const app = express()
 
+const host = "http://localhost:5000"
+
 app.post("/store/checkout/payment/mercadopago/callback/:storeId", async (req, res) => {
     
     const { storeId } = req.params
 
-    const redirectUrl = `http://localhost:5000/store/checkout/payment/mercadopago/callback/${storeId}`
+    const redirectUrl = `${host}/store/checkout/payment/mercadopago/callback/${storeId}`
     
     await fetch(redirectUrl, {
         body: JSON.stringify(req.body),
@@ -15,6 +17,19 @@ app.post("/store/checkout/payment/mercadopago/callback/:storeId", async (req, re
 
     return res.status(200).json()
 })
+
+app.post("/checkout/payment/mercado-pago/callback", async (req, res) => {
+    
+
+    const redirectUrl = `${host}/checkout/payment/mercado-pago/callback`
+    
+    await fetch(redirectUrl, {
+        body: JSON.stringify(req.body),
+    })
+
+    return res.status(200).json()
+})
+
 
 app.listen(80, () => console.log("listening on port 80"))
 
