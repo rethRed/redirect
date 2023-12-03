01954044cff5c32ed3126067307619f541db6301
reth <gabrielwastha@gmail.com>
2023-12-03 19:54:37 -0300

    save

--- src/index.ts
+++ src/index.ts
@@ -29,6 +29,28 @@ app.post("/store/checkout/payment/mercadopago/callback/:storeId", async (req, re
     return res.status(200).json()
 })
 
+
+app.post("/store/checkout/payment/mercadopago/pix/callback/:storeId", async (req, res) => {
+    
+    const { storeId } = req.params
+
+    const redirectUrl = `${host}/store/checkout/payment/mercadopago/pix/callback/${storeId}`
+    
+    console.log(new Date(), req.body,)
+
+    fetch(redirectUrl, {
+        method: "POST",
+        body: JSON.stringify(req.body),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    }).catch(err => console.log(err))
+
+
+    return res.status(200).json()
+})
+
+
 app.post("/checkout/payment/mercado-pago/callback", async (req, res) => {
     
 
